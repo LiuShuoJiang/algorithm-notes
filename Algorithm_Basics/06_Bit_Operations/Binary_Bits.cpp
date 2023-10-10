59addddf3f63d89971b3b11bda2cfda31110f34a
LiuShuoJiang <shuojiang@gatech.edu>
2023-10-10 12:59:13 -0400

    Add bits template code

--- Algorithm_Basics/06_Bit_Operations/Binary_Bits.cpp
+++ Algorithm_Basics/06_Bit_Operations/Binary_Bits.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
 using namespace std;
 
-//返回x的最后一位1
-//树状数组
-//例子：输入二进制(10100)。输出二进制(100)
-//注：-x == ~x + 1   其中~x表示取反
-
-int low_bit(int x) {
-    return x&(-x);
-}
-
 int main() {
     int n = 10;
+    //打印x的每一位
     for (int k = 3; k >= 0; k--) cout << (n >> k & 1);
     cout << endl;
 
